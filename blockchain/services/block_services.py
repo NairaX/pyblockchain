92874b8d7c50f08da356c8faf579e084159d9b25
Sergio de Diego <sergio.dediego@outlook.com>
2018-10-10 23:36:50 +0200

    Create hasher class

---
+++ blockchain/services/block_services.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import json
+
+from hashlib import sha256
+
+
+class Hasher(object):
+
+    @staticmethod
+    def hash(index, timestamp, previous_hash, nonce, difficulty, data):
+        params = {
+            'index': index,
+            'timestamp': timestamp,
+            'previous_hash': previous_hash,
+            'nonce': nonce,
+            'difficulty': difficulty,
+            'data': data,
+        }
+        seed = json.dumps(params, sort_keys=True).encode()
+        return sha256(seed).hexdigest()
