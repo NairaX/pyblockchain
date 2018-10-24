3eb5be60a5adb1950fc1571eb6be962ed0a765e1
Sergio de Diego <sergio.dediego@outlook.com>
2018-10-24 23:28:24 +0200

    Add new block method

--- blockchain/models/block_models.py
+++ blockchain/models/block_models.py
@@ -30,6 +30,24 @@ class Block(object):
     def genesis(cls):
         return cls(1, 'Genesis time', '', 'Genesis hash', 0, 1, 'Genesis block')
 
+    @classmethod
+    def new_block(cls, previous_block, data):
+        nonce = 0
+        index = previous_block.index + 1
+        difficulty = previous_block.difficulty
+        previous_hash = previous_block.hash
+
+        while True:
+            nonce += 1
+            timestamp = int(datetime.utcnow().strftime("%s"))
+            difficulty = cls.set_difficulty(previous_block, timestamp)
+            new_hash = Hasher.hash(index, timestamp, previous_hash, nonce, difficulty, data)
+
+            if new_hash[:difficulty] == '0'*difficulty:
+                break
+
+        return cls(index, timestamp, previous_hash, new_hash, nonce, difficulty, data)
+
     def serialize(self):
         return {
             'Index': self.index,
